94867f594dc8f693ee76018f7cdf35ef7966a85a
Vitalii Kovtun <144720026+vetos5@users.noreply.github.com>
2024-04-09 13:10:07 +0300

    Add files via upload

---
+++ 02_StackImplementation/Source.cpp
@@ -0,0 +1,28 @@
+﻿#include "Stack.h"
+
+
+int main() {
+
+	runTest();
+
+	Stack<int> newStack;
+	newStack.push(1);
+	newStack.push(2);
+	newStack.push(3);
+
+	
+	for (int i = 0; i < 3; i++) {
+		cout << "Stack top element: " << newStack.top() << endl;
+		cout << "Stack size: " << newStack.getSize() << endl;
+		newStack.pop();
+	}
+	if (newStack.isEmpty()) {
+		cout << "Stack is empty." << endl;
+		return 0;
+	}
+	
+	
+	return 0;
+	
+}
+
---
+++ 02_StackImplementation/Stack.h
@@ -0,0 +1,87 @@
+﻿#pragma once
+#include <iostream>
+#include <cassert>
+
+using std::cout;
+using std::endl;
+using std::cerr;
+
+template <typename T>
+class Stack {
+private:
+	T* stack;
+	int size;
+	int capacity;
+
+public:
+	Stack(int capacity = 10) : size(0), capacity(capacity) {
+		stack = new T[capacity];
+	}
+
+	~Stack() {
+		delete[] stack;
+	}
+
+	void push(T element) {
+		if (size < capacity) {
+			stack[size++] = element;
+		}
+		else {
+			cerr << "Stack overflow occured. Couldn't add new element. " << endl;
+		}
+	}
+
+	bool isEmpty() {
+		return size == 0;
+	}
+
+	void pop() {
+		if (!isEmpty()) {
+			--size;
+			
+		}
+		else {
+			cerr << "Stack is empty. " << endl;
+		}
+	}
+
+	T top() {
+		if (!isEmpty()) {
+			return stack[size - 1];
+		}
+		else {
+			cerr << "Stack is empty. Couldn't get top element. " << endl;
+			return T();
+		}
+	}
+
+	int getSize() {
+		return size;
+	}
+
+};
+
+void runTest() {
+
+	Stack<int> newStack;
+	newStack.push(1);
+	newStack.push(2);
+	newStack.push(3);
+
+	assert(newStack.getSize() == 3);
+	assert(newStack.top() == 3);
+
+	newStack.pop();
+	assert(newStack.top() == 2);
+
+	assert(!newStack.isEmpty());
+
+	newStack.pop();
+	newStack.pop();
+	assert(newStack.isEmpty());
+
+	assert(newStack.getSize() == 0);
+
+	cout << "All tests passed. " << endl;
+
+}
